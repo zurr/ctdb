6957a6c44b7b39f20fc9d832ba89392593119259
Spyder <Spyder@localhost>
2010-02-18 16:51:09 +0100

    Devel file added

---
+++ devel/devfixes.sql
@@ -0,0 +1,16 @@
+-- Dev file
+-- Put here all your changes with you Nickname before
+-- Like:
+
+-- Spyder
+-- Missing Class
+UPDATE `creature_template` SET `unit_class`=1 WHERE `entry`=37122;
+UPDATE `creature_template` SET `unit_class`=1 WHERE `entry`=37123;
+UPDATE `creature_template` SET `unit_class`=1 WHERE `entry`=37127;
+UPDATE `creature_template` SET `unit_class`=1 WHERE `entry`=31260;
+UPDATE `creature_template` SET `unit_class`=8 WHERE `entry`=34942;
+UPDATE `creature_template` SET `unit_class`=8 WHERE `entry`=35036;
+UPDATE `creature_template` SET `unit_class`=8 WHERE `entry`=35041;
+UPDATE `creature_template` SET `unit_class`=8 WHERE `entry`=35052;
+UPDATE `creature_template` SET `unit_class`=2 WHERE `entry`=37025;
+UPDATE `creature_template` SET `unit_class`=1 WHERE `entry`=37665;
