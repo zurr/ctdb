e44c4d70ef30ac7a21ea3686909e4609dae80743
ramm8313 <ramm8313@gmail.com>
2010-02-22 09:51:29 -0500

    New forum fixes accepted until 22/02/2010

---
+++ Forum fixes/004F09 - The Light of Dawn (Fix).sql
@@ -0,0 +1,33 @@
+-- NPC: http://www.wowhead.com/?npc=29174
+-- This NPC is on the world but need to be summoning by the event script
+DELETE FROM `creature`WHERE `guid`=116851 AND `id`=29174;
+DELETE FROM `creature`WHERE `guid`=116848 AND `id`=29174;
+DELETE FROM `creature`WHERE `guid`=116850 AND `id`=29174;
+DELETE FROM `creature`WHERE `guid`=116849 AND `id`=29174;
+-- NPC: http://www.wowhead.com/?npc=29177
+-- This NPC is on the world but need to be summoning by the event script
+DELETE FROM `creature`WHERE `guid`=114625 AND `id`=29177;
+-- NPC: http://www.wowhead.com/?npc=29178
+-- This NPC is on the world but need to be summoning by the event script
+DELETE FROM `creature`WHERE `guid`=116879 AND `id`=29178;
+-- NPC: http://www.wowhead.com/?npc=29181
+-- This NPC is on the world but need to be summoning by the event script
+DELETE FROM `creature`WHERE `guid`=116884 AND `id`=29181;
+-- NPC: http://www.wowhead.com/?npc=29176
+-- This NPC is on the world but need to be summoning by the event script
+DELETE FROM `creature`WHERE `guid`=116875 AND `id`=29176;
+-- NPC: http://www.wowhead.com/?npc=29180
+-- This NPC is on the world but need to be summoning by the event script
+DELETE FROM `creature`WHERE `guid`=116854 AND `id`=29180;
+-- NPC: http://www.wowhead.com/?npc=29177
+-- This NPC is on the world but need to be summoning by the event script
+DELETE FROM `creature`WHERE `guid`=116885 AND `id`=29182;
+-- NPC: http://www.wowhead.com/?npc=29179
+-- This NPC is on the world but need to be summoning by the event script
+DELETE FROM `creature`WHERE `guid`=116878 AND `id`=29179;
+-- NPC: http://www.wowhead.com/?npc=29175
+-- This NPC is on the world but need to be summoning by the event script
+DELETE FROM `creature`WHERE `guid`=116863 AND `id`=29175;
+-- NPC: http://www.wowhead.com/?npc=29190
+-- This NPC is on the world but need to be summoning by the event script
+DELETE FROM `creature`WHERE `guid`=116858 AND `id`=29190; 
