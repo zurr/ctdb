d8eb16e17357a4881e626a43d8d52290cefdad9a
ramm8313 <ramm8313@gmail.com>
2010-02-23 15:42:11 -0500

    Corregidos algunos errores

--- Forum fixes/004F16 - Quest gateways both horde and alliance.sql
+++ Forum fixes/004F16 - Quest gateways both horde and alliance.sql
@@ -8,6 +8,6 @@ INSERT INTO `spell_script_target` (`entry`,`type`,`targetEntry`) VALUES
 (33655,1,19292);
 UPDATE `creature_template` SET `AIName`= 'EventAI' WHERE `entry` IN (19291,19292); -- set creatures to use EAI
 DELETE FROM `creature_ai_scripts` WHERE `creature_id` IN (19291,19292); -- EventAI to give credit on spell hit
-INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_inverse_phase_mask`,`event_chance`,`even?t_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action1_ty?pe`,`action1_param1`,`action1_param2`,`action1_param3`,`action2_type`,`action2_p?aram1`,`action2_param2`,`action2_param3`,`action3_type`,`action3_param1`,`action?3_param2`,`action3_param3`,`comment`) VALUES 
+INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_inverse_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action1_type`,`action1_param1`,`action1_param2`,`action1_param3`,`action2_type`,`action2_param1`,`action2_param2`,`action2_param3`,`action3_type`,`action3_param1`,`action3_param2`,`action3_param3`,`comment`) VALUES 
 (1929100,19291,8,0,100,0,33655,-1,0,0,33,19291,6,0,0,0,0,0,0,0,0,0, 'use eai to get credit from spell hit'),
 (1929200,19292,8,0,100,0,33655,-1,0,0,33,19292,6,0,0,0,0,0,0,0,0,0, 'use eai to get credit from spell hit');
