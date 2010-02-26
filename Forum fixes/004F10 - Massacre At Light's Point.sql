778aeef1ec59009404b3d7ae6dd2c819cda28835
Stalker Riddick <Stalker Riddick@localhost>
2010-02-26 16:33:16 +0100

    Proper creature_formations table data.

---
+++ Forum fixes/004F10 - Massacre At Light's Point.sql
@@ -0,0 +1,10 @@
+-- Massacre At Light's Point http://www.wowhead.com/?quest=12701
+-- This npc: http://www.wowhead.com/?npc=28817 need to be unassailable  form the NPC's on the area
+DELETE FROM `creature_template` WHERE `entry`='28817';
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction_A`, `faction_H`, `npcflag`, `speed`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `Health_mod`, `Mana_mod`, `Armor_mod`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `equipment_id`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `WDBVerified`) VALUES
+('28817','0','0','0','0','0','25703','25572','0','0','Mine Car','','','0','1','1','0','35','35','0','1','1','0','0','0','0','0','1','2000','0','1','0','0','0','0','0','0','0','0','0','0','10','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','87','0','0','','0','3','1','1','1','0','0','0','0','0','0','0','0','1','0','0','0','npc_scarlet_miner_cart','0');
+
+-- Need also some model info to be unassailable
+DELETE FROM `creature_model_info` WHERE `modelid`='25703';
+INSERT INTO `creature_model_info` (`modelid`, `bounding_radius`, `combat_reach`, `gender`, `modelid_other_gender`) VALUES
+('25703','0.65','0.65','2','0'); 
---
+++ Forum fixes/004F16 - Quest gateways both horde and alliance.sql
@@ -0,0 +1,13 @@
+-- Fix Quest: gateways both horde and alliance
+-- did not get mission credit for using bomb on gateways
+-- http://www.wowhead.com/?quest=10146
+-- http://www.wowhead.com/?quest=10129
+DELETE FROM `spell_script_target` WHERE `entry`=33655; -- fix spell_target set to correct creature
+INSERT INTO `spell_script_target` (`entry`,`type`,`targetEntry`) VALUES 
+(33655,1,19291),
+(33655,1,19292);
+UPDATE `creature_template` SET `AIName`= 'EventAI' WHERE `entry` IN (19291,19292); -- set creatures to use EAI
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` IN (19291,19292); -- EventAI to give credit on spell hit
+INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_inverse_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action1_type`,`action1_param1`,`action1_param2`,`action1_param3`,`action2_type`,`action2_param1`,`action2_param2`,`action2_param3`,`action3_type`,`action3_param1`,`action3_param2`,`action3_param3`,`comment`) VALUES 
+(1929100,19291,8,0,100,0,33655,-1,0,0,33,19291,6,0,0,0,0,0,0,0,0,0, 'use eai to get credit from spell hit'),
+(1929200,19292,8,0,100,0,33655,-1,0,0,33,19292,6,0,0,0,0,0,0,0,0,0, 'use eai to get credit from spell hit');
