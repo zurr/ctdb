b6f97ab6e2e0dacef69ff64837a6a36c20ed29f4
Spyder <Spyder@localhost>
2010-04-10 00:41:39 +0200

    Release 0.0.7

--- forum_fixes/007F03 - [NPC] Apothecary Hummel.sql
+++
@@ -1,3 +0,0 @@
--- Apothecary Frye, Apothecary Hummel, Apothecary Baxter bosses health is too high
--- By Voltio
-UPDATE `creature_template` SET `Health_mod`=20 WHERE `entry` IN (36272,36296,36565); 
