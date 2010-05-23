90b248a7cd1fb7717a95fee426f8fd6d73a027b3
Spyder <Spyder@localhost>
2010-05-23 19:57:08 +0200

    cleanup

--- forum_fixes/010F02 - [FIX] Children's Week Date & Time.sql
+++
@@ -1,5 +0,0 @@
--- [FIX] Children's Week Date & Time
--- http://www.wowhead.com/event=201
--- By make_the_king
-
-UPDATE `game_event` SET `start_time`='2010-05-02 00:02:00',`end_time`='2020-05-09 00:02:00' WHERE `holiday`='201';
